5d585cfefe05a64aded1f5a377fab0e341693155
smathew54 <88407507+smathew54@users.noreply.github.com>
2025-06-12 21:59:20 -0500

    adding all the code for the hw assignment

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log("am i connect?");
+
+//vanilla,vanilla,vanilla,strawberry,coffee,coffee
+let flavors = prompt("Please enter your flavors here: ");
+
+console.log(flavors);
+
+//flavorList = ["vanilla", "vanilla", "vanilla", "strawberry", "coffee", "coffee"]
+const flavorCounter = (flavors) => {
+  const obj = {};
+  console.log("am i coding?");
+  let flavorList = flavors.split(",");
+  console.log(flavorList);
+  for (let i = 0; i < flavorList.length; i++) {
+    console.log(flavorList[i]);
+    if (flavorList[i] in Object.keys(obj)) {
+      obj[flavorList[i]] += 1;
+    } else {
+      //obj.flavorList[i] = 1;
+      obj[flavorList[i]] = i + 1;
+    }
+
+    console.log(obj);
+  }
+};
+
+console.log(flavorCounter(flavors));
+
+//for (const key in dinner) {
+//    console.log(`${key}: $ ${dinner[key]}`);
